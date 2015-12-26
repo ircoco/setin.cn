58aeff306b0ac6645a347867bc6b51b15b92f2cc
hustcc <i@hust.cc>
2015-12-26 10:39:37 +0800

    update db config

--- inc/mysql.inc.php
+++ inc/mysql.inc.php
@@ -16,8 +16,8 @@ class Mysql {
 	public function __construct() {
 		try {
 			if (empty(Mysql::$CONN)) {
-				Mysql::$CONN = mysql_connect(SAE_MYSQL_HOST_M . ':' . SAE_MYSQL_PORT, SAE_MYSQL_USER, SAE_MYSQL_PASS);
-				mysql_select_db(SAE_MYSQL_DB, Mysql::$CONN);
+				Mysql::$CONN = mysql_connect('127.0.0.1:3306', 'root', 'root');
+				mysql_select_db('setin', Mysql::$CONN);
 				mysql_query('set names utf8');
 			}
 		} catch (Exception $e) {
